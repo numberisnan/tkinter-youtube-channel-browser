006ff57bf3e3b363b572e53ef8aaf9461733486b
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-01-22 22:30:34 -0500

    Fix linting issue from wildcard import

--- dialogues.py
+++ dialogues.py
@@ -1,22 +1,22 @@
-from tkinter import *
+import tkinter
 
 # Search screen
 def searchQueryDialogue():
     "Show search query dialogue and return what was entered"
 
-    root = Tk()
-    searchScreen = Canvas(root, width = 800, height = 600, bg = 'red')
+    root = tkinter.Tk()
+    searchScreen = tkinter.Canvas(root, width = 800, height = 600, bg = 'red')
     searchScreen.pack()
 
-    searchQuery = StringVar()
+    searchQuery = tkinter.StringVar()
     font = ("Consolas", "30")
     smallFont = ("Consolas", "19")
 
-    serchbar = searchScreen.create_window(400, 400, window=Entry(searchScreen,fg = "grey", textvariable = searchQuery, width=30, font=font, justify=CENTER))
-    youtubeImage = PhotoImage(file="images/searchbox/youtubeicon.gif")
-    youtubeIcon = searchScreen.create_image(400,175,image=youtubeImage)
-    title = searchScreen.create_text(400,325,text="Tkinter Youtube Channel Browser",font=font, fill="white")
-    searchButton = searchScreen.create_window(720, 400, window= Button(searchScreen, text="Go",command=root.destroy, font=smallFont))
+    searchScreen.create_window(400, 400, window=tkinter.Entry(searchScreen,fg = "grey", textvariable = searchQuery, width=30, font=font, justify=tkinter.CENTER))
+    youtubeImage = tkinter.PhotoImage(file="images/searchbox/youtubeicon.gif")
+    searchScreen.create_image(400,175,image=youtubeImage)
+    searchScreen.create_text(400,325,text="Tkinter Youtube Channel Browser",font=font, fill="white")
+    searchScreen.create_window(720, 400, window= tkinter.Button(searchScreen, text="Go",command=root.destroy, font=smallFont))
 
     root.mainloop()
     return searchQuery.get()
