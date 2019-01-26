961f349d07c6c217abcb0f99ba7771b397283d9c
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-01-26 11:32:13 -0500

    Standerdised import statement

--- dialogues.py
+++ dialogues.py
@@ -1,22 +1,22 @@
-import tkinter
+import tkinter as tk
 
 # Search screen
 def searchQueryDialogue():
     "Show search query dialogue and return what was entered"
 
-    root = tkinter.Tk()
-    searchScreen = tkinter.Canvas(root, width = 800, height = 600, bg = 'red')
+    root = tk.Tk()
+    searchScreen = tk.Canvas(root, width = 800, height = 600, bg = 'red')
     searchScreen.pack()
 
-    searchQuery = tkinter.StringVar()
+    searchQuery = tk.StringVar()
     font = ("Consolas", "30")
     smallFont = ("Consolas", "19")
 
-    searchScreen.create_window(400, 400, window=tkinter.Entry(searchScreen,fg = "grey", textvariable = searchQuery, width=30, font=font, justify=tkinter.CENTER))
-    youtubeImage = tkinter.PhotoImage(file="images/searchbox/youtubeicon.gif")
+    searchScreen.create_window(400, 400, window=tk.Entry(searchScreen,fg = "grey", textvariable = searchQuery, width=30, font=font, justify=tk.CENTER))
+    youtubeImage = tk.PhotoImage(file="images/searchbox/youtubeicon.gif")
     searchScreen.create_image(400,175,image=youtubeImage)
     searchScreen.create_text(400,325,text="Tkinter Youtube Channel Browser",font=font, fill="white")
-    searchScreen.create_window(720, 400, window= tkinter.Button(searchScreen, text="Go",command=root.destroy, font=smallFont))
+    searchScreen.create_window(720, 400, window= tk.Button(searchScreen, text="Go",command=root.destroy, font=smallFont))
 
     root.mainloop()
     return searchQuery.get()
