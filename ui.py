349a69af8c3461577e9e0047d26a5f2bad0bc315
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-02-15 20:20:30 -0500

    Add details ui

--- ui.py
+++ ui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import ytrequests
+import ytrequests, utils, copy, re
 
 # Search screen
 def searchUI(**kwargs):
@@ -25,7 +25,52 @@ def searchUI(**kwargs):
     root.mainloop()
     return searchQuery.get()
 
+def helpUI(message, **kwargs):
+    "Show help message"
+    width = kwargs.get('width') or 800
+    height = kwargs.get('height') or 100
+    font = ("Leelawadee", "23")
+
+    root = tk.Tk()
+    root.title("Help")
+    root.geometry("%dx%d+%d+%d" % (width, height, 0, 0))
+    window = tk.Canvas(root, width=width, height=height, bg='white')
+    window.pack()
+
+    window.create_text(width//2,height//2,text=message, font=font)
+
+    return root
+
+def detailsUI(channelID, **kwargs):
+    "Requests and shows details of specified channel"
+    width = kwargs.get('width') or 600
+    height = kwargs.get('height') or 600
+    font = ("Leelawadee", "23")
+    apikey = utils.readfile("apikey.key")
+
+    # Request channel data
+    channelData = ytrequests.youtubeChannelRequest(channelID, apikey=apikey)["items"][0]
+
+    snippet = channelData["snippet"]
+    topicDetails = channelData["topicDetails"]
+    statistics = channelData["statistics"]
+
+    root = tk.Tk()
+    root.title(snippet["title"])
+    window = tk.Canvas(root, width=width, height=height, bg='white')
+    window.pack()
+
+    imageURL = snippet["thumbnails"]["default"]["url"]
+
+    thumbnailImage = ytrequests.requestTkImage(imageURL, apikey=apikey)
+
+    image = window.create_image(width/2, 100, image=thumbnailImage)
+
+    tk.mainloop()
+
 def searchResultsUI(data={}, **kwargs):
+    "Show search results with thumbnails and descriptions"
+
     width = kwargs.get('width') or 500
     height = kwargs.get('height') or 600
     font = ("Leelawadee", "23")
@@ -42,6 +87,9 @@ def searchResultsUI(data={}, **kwargs):
     window = tk.Canvas(root, width=width, height=height, bg='white')
     window.pack()
 
+    def onclick_helper(snippet):
+        return lambda e:detailsUI(snippet["channelId"])
+
     for i in range(resultsNumber): # Generate search results
         # Relative x and y for (0,0) in mini-box
         y = i*resultsHeight
@@ -56,30 +104,14 @@ def searchResultsUI(data={}, **kwargs):
 
         window.create_rectangle(x,y,x+resultsWidth,y+resultsHeight, fill='grey' if i % 2 == 0 else 'white') #Draw frome
 
-        
         imagedata.insert(i,ytrequests.requestTkImage(thumburl)) # Request thumbnail
-        
-        clickableImage = window.create_image(x+resultsWidth/5.5, y+resultsHeight/2, image=imagedata[i]) # Add image
-        window.tag_bind(clickableImage, "<ButtonPress-1>", print)
+
+        clickableimage = window.create_image(x+resultsWidth/5.5, y+resultsHeight/2, image=imagedata[i]) # Add image
+        window.tag_bind(clickableimage, "<ButtonPress-1>", onclick_helper(snippet)) # Bind thumbnail with results ui
 
         window.create_text(x+resultsWidth/2.5,y+resultsHeight/5,text=channelName, font=font, anchor=tk.W, width=str(resultsWidth/2.5) + "p") # Write channel name
 
         window.create_text(x+resultsWidth/2.5,y+resultsHeight/1.5,text=description.strip(), font=smallFont, anchor=tk.W, width=str(resultsWidth/2.5) + "p") # Write channel description
-    helpUI()
+
+    helpUI("To see details for each channel, click the thumbnail")
     root.mainloop()
-    
-
-def helpUI(**kwargs):
-    width = kwargs.get('width') or 800
-    height = kwargs.get('height') or 100
-    font = ("Leelawadee", "23")
-
-    root = tk.Tk()
-    root.title("Help")
-    root.geometry("%dx%d+%d+%d" % (width, height, 0, 0))
-    window = tk.Canvas(root, width=width, height=height, bg='white')
-    window.pack()
-
-    window.create_text(width//2,height//2,text="To see details for each channel, click the thumbnail", font=font)
-
-    return root
--- ytrequests.py
+++ ytrequests.py
@@ -3,10 +3,11 @@ from PIL import ImageTk, Image
 
 def youtubeSearchRequest(query, **kwargs):
     "Make youtube search request and return response"
-    maxResults = str(kwargs.get("maxResults") or "3")
+    maxResults = str(kwargs.get("maxResults") or 3)
     apikey = kwargs.get("apikey")
     
     requestString = "https://www.googleapis.com/youtube/v3/search/?part=snippet&maxResults=" + maxResults + "&q=" + query + "&type=channel&key=" + apikey
+    
     try:
         response = requests.get(requestString).json()
     except:
@@ -14,11 +15,27 @@ def youtubeSearchRequest(query, **kwargs):
     
     return response
 
-def requestTkImage(url):
+def requestTkImage(url, **kwargs):
     "Downloads image, returns tkinter.PhotoImage instance"
+    width = kwargs.get("width") or 150
+    height = kwargs.get("height") or 150
+
     try:
         response = requests.get(url)
     except:
         return requestTkImage("http://127.0.0.1:8887/response_Thumbnail_.jpg") #Try local source for testing if no internet TODO Add actual error handling
     
-    return ImageTk.PhotoImage(image=Image.open(io.BytesIO(response.content)).resize((150,150), Image.ANTIALIAS))
+    return ImageTk.PhotoImage(image=Image.open(io.BytesIO(response.content)).resize((width,height), Image.ANTIALIAS))
+
+def youtubeChannelRequest(channelID, **kwargs):
+    "Make youtube channel detail request and return response"
+    apikey = kwargs.get("apikey")
+
+    requestString = "https://www.googleapis.com/youtube/v3/channels?part=%s&id=%s&key=%s" % ("snippet,statistics,topicDetails", channelID, apikey)
+
+    try:
+        response = requests.get(requestString).json()
+    except:
+        response = json.loads(utils.readfile("APIResponses/response_MrBeast_Channel.json")) #TODO Return actual error response
+    
+    return response
